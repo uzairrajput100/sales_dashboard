c4f2ab3b88c8ad8d958b3ff69d10d9e3a06bf0f4
Uzair Ahmed <uzairrajput100@gmail.com>
2024-06-19 10:41:36 +0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -163,7 +163,8 @@ if fl is not None:
         st.markdown("Month wise Sub-Category Table")
         filtered_df["month"] = filtered_df["Order Date"].dt.month_name()
         sub_category_year = pd.pivot_table(data=filtered_df, values="Sales", index=["Sub-Category"], columns="month")
-        st.write(sub_category_year.style.background_gradient(cmap="Blues"))
+        st.write(sub_category_year.style.background_gradient(gmap=df_log.div(df_log.max()),
+                             low=-0.3, cmap=cm, axis=None)
 
     data1 = px.scatter(filtered_df, x="Sales", y="Profit", size="Quantity", title="Relationship between Sales and Profit using Scatter Plot")
     st.plotly_chart(data1, use_container_width=True)
